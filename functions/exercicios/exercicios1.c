f094e1265b13e1797a684d37d81b97a0d6cb6233
syo77 <yurivsv77@gmail.com>
2024-10-21 11:13:08 -0300

    apc2

---
+++ functions/exercicios/exercicios1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int abs(int n) {
+
+    if(n<0) n = -n;
+   
+    return 0;
+}
+
+int is_digit_v2(char c) {
+    char digitos[] = "0123456789";
+    for(int i = 0; digitos[i] != '\0'; i++) {
+        if(c == digitos[i]) return 1;
+    }
+}
+
+int is_not_alphabet(char c) {
+
+}
+
+void exibir_horas(int milisegundos) {
+int horas
+}
+
+
+
+int main () {
+
+
+
+
+
+    return 0 ;
+}
